0b70480fa9ef3f969963187f1e8d715dc45bf104
Tom Coleman <tom@thesnail.org>
2015-03-16 16:35:16 +1100

    Subclass Subscription. Tests pass. Woo

--- packages/publish-joins/package.js
+++ packages/publish-joins/package.js
@@ -9,6 +9,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.2');
+  api.use('underscore');
   api.addFiles('subscription-tree.js', 'server');
   api.addFiles('publish-joins.js', 'server');
   
--- packages/publish-joins/subscription-tree.js
+++ packages/publish-joins/subscription-tree.js
@@ -10,55 +10,71 @@ var Subscription;
 //     the sub is stopped
 //   - child subs pass added/etc/ready to parent sub
 //   - parent sub is only ready when all children + itself are ready.
-SubscriptionTree = function(parent, notRoot) {
+SubscriptionTree = function(parentSubscription, notRoot) {
   var self = this;
-
-  var children = [];
-  // is this particular subscription ready?
-  var ready = false;
-  // is the entire tree of this and it's children ready?
-  var treeReady = false;
-  var stopCbs = [];
   
+  // this will definitely work the first time ;)
   if (! notRoot) {
-    Subscription = Subscription || parent.constructor;
+    setSubscriptionPrototype(parentSubscription.constructor);
   }
   
-  // FIXME: does this do the right thing in the case of error / stop?
-  self.publish = function(handler) {
+  // it's too late to call Meteor._inherits so let's try this slightly wacky
+  //   approach
+  _.extend(this.constructor.prototype, Subscription.prototype, subscriptionTreeMethods);
+  
+  
+  // XXX: should we generate a name like flips.0.1?, likewise a _subscriptionId?
+  //   for the moment all names are undefined, this will appear a universal sub
+  Subscription.call(self, parentSubscription._session);
+  
+  self._root = ! notRoot;
+  self.parent = parentSubscription;
+  self._children = [];
+  // is this particular subscription ready?
+  self._nodeReady = false;
+  // is the entire tree of this and it's children ready?
+  self._treeReady = false;
+
+  self.parent.onStop(function() {
+    self.stop();
+  });
+}
+
+var subscriptionTreeMethods = {
+  // TODO: is this the best name?
+  publish: function(handler) {
     // TODO: should we not be so hacky here?
-    self._handler = handler;
-    self._params = Array.prototype.slice.call(arguments, 1);
-    Subscription.prototype._runHandler.call(self);
-  }
+    this._handler = handler;
+    this._params = Array.prototype.slice.call(arguments, 1);
+    this._runHandler();
+  },
   
-  self.publishChild = function(handler /*, arguments */) {
-    var child = new SubscriptionTree(self, true);
-    children.push(child);
+  publishChild: function(handler /*, arguments */) {
+    var child = new SubscriptionTree(this, true);
+    this._children.push(child);
     child.publish.apply(child, arguments);
     return child;
-  }
-  
-  self._isTreeReady = function() {
-    return treeReady;
-  }
-  self._checkTreeReady = function() {
-    var allChildrenReady = _.all(children, function(child) {
-      return child._isTreeReady();
+  },
+
+  _checkTreeReady: function() {
+    var allChildrenReady = _.all(this._children, function(child) {
+      return child._treeReady;
     });
-    treeReady = (allChildrenReady && ready);
-    if (treeReady) { 
-      if (notRoot) {
-        parent._checkTreeReady();
+    this._treeReady = (allChildrenReady && this._nodeReady);
+    if (this._treeReady) { 
+      if (this._root) {
+        this.parent.ready();
       } else {
-        parent.ready();
+        this.parent._checkTreeReady();
       }
     }
-  };
+  },
   
-  // TODO: could take fields as an argument to allow only responding to changes
-  //   to relevant fields
-  self.forEach = function(cursor, handler) {
+  // TODO: could take fields as an argument to allow only responding to
+  //    changes to relevant fields
+  forEach: function(cursor, handler) {
+    var self = this;
+    
     // : _id => SubscriptionTree
     var forEachChildren = {};
     var addChild = function(id, doc) {
@@ -71,17 +87,17 @@ SubscriptionTree = function(parent, notRoot) {
       }, 
       changed: function(id, mod) {
         // if we had fields above, we could short-circuit in some cases here.
-        
+      
         // TODO: are there any conditions under which we can re-use the old sub?
         //   - Not in general
         //   - If old + new both returned cursors, could we check if they are
         //       the same? Or is that a pointless optimization?
         var oldChild = forEachChildren[id];
-        
+      
         // TODO: there should be an official API to do this?
         var doc = handle._multiplexer._cache.docs.get(id);
         addChild(id, doc);
-        
+      
         // We stop the old child after we start the new sub, so if there
         //   are common cursors, we don't teardown and re-establish the
         //   observers -- we just gracefully re-use the old one.
@@ -94,78 +110,23 @@ SubscriptionTree = function(parent, notRoot) {
         self._checkTreeReady();
       }
     });
-  };
+  },
   
-  self.stop = function() {
-    // self._namedSubs[subId]._removeAllDocuments();
-    // self._namedSubs[subId]._deactivate();
-  };
-  
-  // proxy these right through to the parent for now
-  _.each(['userId', 'connection'], function(prop) {
-    self[prop] = parent[prop];
-  });
-  _.each(['added', 'changed', 'removed', 'error', '_isDeactivated'], function(prop) {
-    self[prop] = parent[prop].bind(parent);
-  });
-  
-  self.onStop = function(fn) {
-    stopCbs.push(fn);
+  ready: function() {
+    this._nodeReady = true;
+    this._checkTreeReady();
+  },
+
+  stop: function() {
+    this._removeAllDocuments();
+    this._deactivate();
   }
-  self.ready = function() {
-    ready = true;
-    self._checkTreeReady();
+};
+
+var setSubscriptionPrototype = function(s) {
+  if (Subscription) {
+    return;
   }
-  
-  parent.onStop(function() {
-    self.stop();
-  });
+   
+  Subscription = s;
 }
-
-
-
-
-// XXX: I think the below is redundant
-// // PROBLEM: diamond joins
-// //
-// // Fix: use a "merge subscriptions" wrapper around a sub, which does it's own
-// //   merge box (or something less efficient?)
-// //
-// // Something like [this is the inefficient version]:
-// allowDiamonds: function(sub) {
-//   var docCounts = {};
-//   return {
-//     added: function(id, doc) {
-//       if (! docCounts[id]) {
-//         docCounts[id] = 0;
-//         sub.added(id, doc);
-//       } else {
-//         // NOTE: the issue here is that we may already know about some of
-//         //   these fields of doc (usually all). Still we have an upstream
-//         //   mergebox that deals with this.
-//         sub.changed(id, {$set: doc});
-//       }
-//       docCounts[id] += 1;
-//     },
-//     removed: function(id) {
-//       docCounts[id] -= 1;
-//       if (docCounts[id] === 0) {
-//         sub.removed(id);
-//       }
-//       // NOTE: the issue here is that we possibly should be removing fields
-//       //   from the doc right now (ie issuing a changed) if we haven't removed
-//       //   it (in the general case). Still it's probably not a huge issue.
-//       //   (to do so we'd have to implement a second merge box which seems
-//       //    crazy). Is there some way we can hijack the real mergebox better?
-//       // TODO: look into this
-//     },
-//     changed: sub.changed,
-//     ready: sub.ready
-//   }
-// }
-//
-// //
-// // - how to deal with Egor's circular dependencies issue?
-// //   - A -> adds B
-// //   - B -> add C
-// // TODO: think about this
